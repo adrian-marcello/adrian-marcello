596590c20afacefe02dafd00c1913d55704dac43
adrian-marcello <adrianmarcellodias@gmail.com>
2023-05-19 09:09:43 -0300

    js no html

--- clock.js
+++ clock.js
@@ -1,11 +1,11 @@
-// contaData()
+contaData()
 
 
-// function contaData() {
-//     var now = new Date();
-//     var she = new Date(2023,6,2);
+function contaData() {
+    var now = new Date();
+    var she = new Date(2023,6,2);
 
-//     var h2 = document.getElementById("h2");
-//     h2.innerHTML = Math.floor((she.getTime()-now.getTime())/86400000);
-// }
+    var h2 = document.getElementById("h2");
+    h2.innerHTML = Math.floor((she.getTime()-now.getTime())/86400000);
+}
 console.log("teste")
